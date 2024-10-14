fa94712d2c105ff34d1c015a669f29c2193be9b4
AngeloR32 <90225587+AngeloR32@users.noreply.github.com>
2024-10-13 21:21:16 -0400

    cositas

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';  // Your landing page component
 import Homepage from './Homepage';  // The original page
-import getstarter from './getstarter';  // The Sample Melodies page
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />           {/* Landing Page */}
-        <Route path="/app" element={<Homepage />} />           {/* Original Page */}
-        <Route path="/getstarter" element={<getstarter />} /> {/* Sample Melodies */}
+        <Route path="/app" element={<Homepage />} />   {/* Original Page */}
+
       </Routes>
     </Router>
   );
