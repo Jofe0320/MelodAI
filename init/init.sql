bbb26aaaa11d5c9bf8649ff5db321296c3caf932
Jofe0320 <jose.ortiz107@upr.edu>
2024-10-14 18:09:01 -0400

    Commit For sign In

--- init/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
-);
-
-INSERT INTO users (name, email) VALUES
-('John Doe', 'john.doe@example.com'),
-('Jane Smith', 'jane.smith@example.com');
