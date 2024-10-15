9073e567260c664728412278dbe8b784a87cc0a2
AngeloR32 <90225587+AngeloR32@users.noreply.github.com>
2024-10-15 14:45:39 -0400

    explore page
    hice un cambio del get started a el explore page que es pa ver como se juega con el tipo de musica

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';  // Your landing page component
 import Homepage from './Homepage';  // The original page
+import ExplorePage from "./getstarter";  // New Explore page
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />           {/* Landing Page */}
         <Route path="/app" element={<Homepage />} />   {/* Original Page */}
+          <Route path="/explore" element={<ExplorePage />} />
 
       </Routes>
     </Router>
